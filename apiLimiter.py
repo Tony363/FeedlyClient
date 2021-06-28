ea9067c58b02c9d050dcafa3740d409351e478ad
pysolver33 <pysolver33@gmail.com>
2021-06-28 22:16:58 +0800

    next task

---
+++ apiLimiter.py
@@ -0,0 +1,23 @@
+import aioredis
+import uvicorn
+from fastapi import Depends, FastAPI
+
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    redis = await aioredis.create_redis_pool("redis://localhost")
+    FastAPILimiter.init(redis)
+
+
+@app.get("/", dependencies=[Depends(RateLimiter(times=2, seconds=5))])
+async def index():
+    return {"msg": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", debug=True, reload=True)
--- opml_parse.py
+++ opml_parse.py
@@ -2,6 +2,9 @@ import pandas as pd
 import opml as ol
 import listparser as lp
 import json
+from dict2xml import dict2xml
+from dicttoxml import dicttoxml
+from xml.dom.minidom import parseString
 
 def OPML():
     outline = ol.parse(
@@ -38,11 +41,22 @@ def Lparser(rssfile='./feedly-e42affb2-52f5-4889-8901-992e3a3e35de-2021-06-28.op
     if to_csv: df.to_csv('feedlyRSS.csv')
     return df
 
-def RSSmultiple(*args):
+def RSSmultiple(*args,source='url'):
     df = pd.DataFrame()
     for file in args:
         df = pd.concat([df,Lparser(file)],axis=0)
+    df.sort_values(by=source,inplace=True)
+    df.set_index(source,inplace=True)
     return df
+
+def to_xml(df,orient='list'):
+    xml = dicttoxml(df.to_dict(orient=orient),attr_type=False,item_func=lambda x:x)
+    xml_decode = xml.decode()
+    xmlfile = open("FeedlyRSS.xml","w")
+    xmlfile.write(xml_decode)
+    xmlfile.close()
+    return parseString(xml)
+
 
 if __name__ == '__main__':
     print(pd.__version__)
@@ -51,6 +65,8 @@ if __name__ == '__main__':
         './feedly-e42affb2-52f5-4889-8901-992e3a3e35de-2021-06-28.opml',
         './feedly-e42affb2-52f5-4889-8901-992e3a3e35de-2021-06-28.opml'
         )
-    df.to_csv("feedlyRSS.csv")
+    dom = to_xml(df)
+    print(dom.toprettyxml())
+    # df.to_csv("feedlyRSS.csv")
     pass
     
