58fa716c174bc71a0b710fbbd2328b02357f25ba
pysolver33 <pysolver33@gmail.com>
2021-06-30 21:34:02 +0800

    practicing multiprocessing

--- apiLimit/apiLimiter.py
+++ apiLimit/apiLimiter.py
@@ -1,67 +1,55 @@
-import aioredis,uvicorn,time
+import aioredis,uvicorn,time,logging
 from fastapi import Depends, FastAPI
 from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
-from multiprocessing import Process,Value,Manager
+from fastapi_utils.tasks import repeat_every
 
 
 class Cache:
     def __init__(self):
-        self.s_time = time.time()
+        """self.s_time = time.time()"""
         self.token = 0
-        self.manager = Manager()
-        self.mul_token = self.manager.Value('i',0)
 
     def checkToken(self):
-        print(time.time() - self.s_time)
+        """
+        # for handled by request
         if time.time() - self.s_time >= 60:
             self.s_time = time.time()
             self.token = 0
+        """
         if self.token < 10:
             self.token += 1
             return {'msg':'hello world'} 
         return {'msg':'too many requests received'}
 
-    def checkCalls(self):
-        if self.mul_token.value >= 10:
-            return {'msg':'too many requests received'}
-        self.mul_token.value += 1
-        return {'msg':'hello world'}
-
-    def updateToken(self):
-        print(time.time() - self.s_time >= 60)
-        if time.time() - self.s_time >= 60:
-            self.s_time = time.time()
-            self.mul_token.value = 0
-
-
+logger = logging.getLogger(__name__)
 app = FastAPI()
 cache = Cache()
 
-@app.get('/')
-async def index():
-    return cache.checkCalls()
+@app.on_event("startup")
+@repeat_every(seconds=60,logger=logger,wait_first=True)
+def resetToken():
+    global cache
+    cache.token = 0
 
-# @app.get("/")
-# async def index():
-#     return cache.checkToken()
+@app.get("/")
+async def index():
+    return cache.checkToken()
 
 # @app.on_event("startup")
 # async def startup():
-    # redis = await aioredis.create_redis_pool("redis://127.0.0.1:6379")
-    # await FastAPILimiter.init(redis)
+#     redis = await aioredis.create_redis_pool("redis://127.0.0.1:6379")
+#     await FastAPILimiter.init(redis)
 
-# @app.get("/", dependencies=[Depends(RateLimiter(times=2, seconds=5))])
+# @app.get("/", dependencies=[Depends(RateLimiter(times=10, seconds=60))])
 # async def index():
-#     return {"msg": "Hello World"}@app.get("/")
+#     return {"msg": "Hello World"}
     
     
 if __name__ == "__main__":
-    p1 = Process(target=uvicorn.run("apiLimiter:app", debug=True, reload=True))
-    p1.start()
-    p2 = Process(target=cache.updateToken)
-    p2.start()
-    
-    p1.join()
-    p2.join()
+    """
+    learn leaking bucket algorithm
+    so far using token bucket algorithm 
+    """
+    uvicorn.run("apiLimiter:app", debug=True, reload=True)
     
